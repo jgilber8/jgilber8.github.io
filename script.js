ab212df14e13a5632de66a1b987c6b766471fe95
jgilber8 <74936158+jgilber8@users.noreply.github.com>
2025-06-25 19:04:54 -0400

    image

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 let correct = false;
-while(correct == false){
+
+let element = document.getElemendById("name");
+let theText = element.value;
+console.log(theText);
+/**while(correct == false){
 let answer = prompt("enter a guess");
 let answer_hash = md5(answer);
 if(answer_hash == '49f68a5c8493ec2c0bf489821c21fc3b'){
@@ -7,6 +11,6 @@ if(answer_hash == '49f68a5c8493ec2c0bf489821c21fc3b'){
     correct = true;
 }
 else{
-    console.log("answer_hash");
+    console.log(answer_hash);
 }
-}
+}**/
