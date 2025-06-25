b34cae0af2f65438c7cff984638158e58041069c
jgilber8 <74936158+jgilber8@users.noreply.github.com>
2025-06-25 18:53:28 -0400

    looping prompt

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
-let x = md5('hi');
-console.log(x);
+let correct = false;
+while(correct == false){
+let answer = prompt("enter a guess");
+let answer_hash = md5(answer);
+if(answer_hash == '49f68a5c8493ec2c0bf489821c21fc3b'){
+    console.log("you got it!");
+    correct = true;
+}
+else{
+    console.log("answer_hash");
+}
+}
